7d768f69848369adf17f39af359611fd1aa0937b
GrzegorzStąpała <grzegorz.stapala@gmail.com>
2025-04-17 11:09:19 +0200

    Init Functions

---
+++ AzureFunctions/TestFunction.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctions
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("TestFunction")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+
+			string name = req.Query["name"];
+
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			dynamic data = JsonConvert.DeserializeObject(requestBody);
+			name = name ?? data?.name;
+
+			var method = req.Method;
+
+			return new OkObjectResult($"Welcome to Azure Functions! by method {method}");
+        }
+    }
+}
